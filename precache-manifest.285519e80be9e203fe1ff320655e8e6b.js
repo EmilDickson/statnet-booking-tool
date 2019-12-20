6fbd00287c6b5786eab37274d4d663b1d9c44a26
EmilDickson <emil.dickson@gmail.com>
2019-12-20 18:06:47 +0100

    Updates

---
+++ precache-manifest.285519e80be9e203fe1ff320655e8e6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cdf4f201b791edb8d15de6bfed58f6e",
+    "url": "/statnet-booking-tool/index.html"
+  },
+  {
+    "revision": "ede97e809c63557e96d9",
+    "url": "/statnet-booking-tool/static/css/main.2afe0ae3.chunk.css"
+  },
+  {
+    "revision": "273c25a5c2d53ef259a5",
+    "url": "/statnet-booking-tool/static/js/2.783971eb.chunk.js"
+  },
+  {
+    "revision": "93314d634a079b009aaadd6c2b023ab6",
+    "url": "/statnet-booking-tool/static/js/2.783971eb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ede97e809c63557e96d9",
+    "url": "/statnet-booking-tool/static/js/main.7196aea1.chunk.js"
+  },
+  {
+    "revision": "805daef1799162373733",
+    "url": "/statnet-booking-tool/static/js/runtime-main.56b5e4d7.js"
+  }
+]);
